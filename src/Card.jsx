41b20458b766ac21cf9be1e2a97e363e4bf8af63
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-03-29 21:30:59 -0700

    clean up

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,9 +7,9 @@ export default function Card({ villager }) {
         <img className="house-img" alt={`${villager.Villager}'s house`} src={`img/homes/${villager.Villager} - ${villager.Personality}.png`} />
       </div>
       <div className="container-right">
-        <h3>{villager.Villager}</h3>
-        <h4>{villager.Animal}</h4>
-        <h4>{villager.Personality}</h4>
+        <h1>{villager.Villager}</h1>
+        <h2>{villager.Animal}</h2>
+        <h2>{villager.Personality}</h2>
         <img className="villager-img" alt={`${villager.Villager}'s icon`} src={`img/villagers/${villager.Villager}_NH_Villager_Icon.png`} />
       </div>
     </div>
