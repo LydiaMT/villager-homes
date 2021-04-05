32782f1225cc970ae3e13dfb66c1ad71bdfb05a2
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-04-04 17:57:32 -0700

    rebuild

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,13 +4,13 @@ export default function Card({ villager }) {
   return (
     <div className="Card">
       <div className="container-left">
-        <img className="house-img" alt={`${villager.Villager}'s house`} src={`img/homes/${villager.Villager} - ${villager.Personality}.png`} />
+        <img className="house-img" loading="lazy" alt={`${villager.Villager}'s house`} src={`img/homes/${villager.Villager} - ${villager.Personality}.png`} />
       </div>
       <div className="container-right">
         <h1 className="villager-text">{villager.Villager}</h1>
         <h2 className="villager-text">{villager.Animal}</h2>
         <h2 className="villager-text">{villager.Personality}</h2>
-        <img className="villager-img" alt={`${villager.Villager}'s icon`} src={`img/villagers/${villager.Villager}_NH_Villager_Icon.png`} />
+        <img className="villager-img" loading="lazy" alt={`${villager.Villager}'s icon`} src={`img/villagers/${villager.Villager}_NH_Villager_Icon.png`} />
       </div>
     </div>
   );
